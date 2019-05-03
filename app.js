be7b67f8e11180762d00649acc32667e50e0635e
fslee18 <sandyfslee@gmail.com>
2019-05-03 08:48:45 -0700

    changes for heroku

--- app.js
+++ app.js
@@ -20,6 +20,9 @@ const api = require('./routes/api');
 
 
 var app = express();
+//heroku or 3000
+const port = process.env.PORT || 3000
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -32,6 +35,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 //Set up the router for the courses
 app.use('/', api);
 
+app.listen(port, () => {
+  console.log('Server is up on port' + port)
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
